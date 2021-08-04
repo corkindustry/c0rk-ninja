69c6ba5e5bb47a969ba7c55748c4443e05848b61
jonah <falsefriend13@gmail.com>
2021-08-04 12:00:36 -0400

    next update

--- pages/uhh.tsx
+++ pages/uhh.tsx
@@ -1,4 +1,4 @@
-import styles from '../components/uhh.module.scss'
+import styles from '../styles/uhh.module.scss'
 
 const uhh = () => {
     return (
@@ -8,7 +8,6 @@ const uhh = () => {
                 src="https://discordapp.com/widget?id=607398516360347861&theme=dark"
                 width="350"
                 height="500"
-                allowtransparency="true"
                 frameBorder="0"
                 sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
             />
