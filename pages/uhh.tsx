3e273e63fb106ed8449d956170d5fef0ebf388bb
jonah <falsefriend13@gmail.com>
2021-08-04 14:37:58 -0400

    built the league uhh page.

--- pages/uhh.tsx
+++ pages/uhh.tsx
@@ -1,17 +1,52 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/uhh.module.scss'
+import footFace from '../public/images/face.jpg'
 
 const uhh = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.header}>League Uhh</div>
-            <iframe
-                src="https://discordapp.com/widget?id=607398516360347861&theme=dark"
-                width="350"
-                height="500"
-                frameBorder="0"
-                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-            />
+        <>
+        <Head>
+        <title>League Uhh</title>
+      </Head>
+        <div className={styles.uhh}>
+            <div className={styles.header}><Image src={footFace} width="100.5" height="56.5" /> League Uhh</div>
+            <div className={styles.code}>League code: <a href="https://fantasy.premierleague.com/leagues/auto-join/btpymu">btpymu</a></div>
+            <div className={styles.container}>
+                <div className={styles.section}>
+                    <h3>League Rules</h3>
+                    <ul>
+                        <li>$20 per club</li>
+                        <li>Last place pays an extra $50 at the end of the season</li>
+                        <li>Winner take all!</li>
+                    </ul>
+                    <hr />
+                    <h3>Get Set Up:</h3>
+                    <ol>
+                        <li>Create your squad</li>
+                        <li>Join the league using the league code (above)</li>
+                        <li>Paypal Jonah $20<br/><a href="https://paypal.me/jponeil" target="_blank">paypal.me/jponeil</a></li>
+                        <li>Join the Discord (if you haven't yet)</li>
+                    </ol>
+                </div>
+                <div className={styles.section}>
+                    <h3>Confirmed Clubs:</h3>
+                    <ul>
+                        <li>Goon Gumpas (Jonah O)</li>
+                        </ul>
+                </div>
+                <div>
+                    <iframe
+                        src="https://discordapp.com/widget?id=607398516360347861&theme=dark"
+                        width="350"
+                        height="500"
+                        frameBorder="0"
+                        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                    />
+                </div>
+            </div>
         </div>
+        </>
     )
 }
 
